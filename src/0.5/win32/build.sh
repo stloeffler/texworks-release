bcee5934a4368d9ad03ab778eb7111444ed82d24
Stefan Löffler <st.loeffler@gmail.com>
2015-04-03 19:56:30 -0400

    Use Qt5 in Windows builds

--- src/0.5/win32/build.sh
+++ src/0.5/win32/build.sh
@@ -31,7 +31,7 @@ mkdir build && cd build
 cmake .. \
 	-DCMAKE_BUILD_TYPE="Release" \
 	-DTW_BUILD_ID='official' \
-	-DDESIRED_QT_VERSION=4 \
+	-DDESIRED_QT_VERSION=5 \
 	-DCMAKE_TOOLCHAIN_FILE="${MXE_DIR}/usr/i686-w64-mingw32/share/cmake/mxe-conf.cmake" \
 	-DCMAKE_PREFIX_PATH="${MXE_DIR}/usr/i686-w64-mingw32/qt5/lib/cmake" \
 	-DTEXWORKS_ADDITIONAL_LIBS="imm32;opengl32;lcms;jpeg;tiff;png;lzma;freetype;bz2;pcre16;sicuuc;sicudt;wsock32;winmm"
